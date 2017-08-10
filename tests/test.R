c04e798907cd87131dbb7017887c30ae86d770b8
Anthony Damico <ajdamico@gmail.com>
2017-08-10 03:45:29 -0400

    pof draft

--- tests/test.R
+++ tests/test.R
@@ -6,55 +6,120 @@ pof_cat <-
 	get_catalog( "pof" ,
 		output_dir = file.path( getwd() ) )
 
-# 2015 only
-pof_cat <- subset( pof_cat , year == 2015 )
+# 2008-2009 only
+pof_cat <- subset( pof_cat , period == "2008-2009" )
 # download the microdata to your local computer
 stopifnot( nrow( pof_cat ) > 0 )
 
+options( survey.lonely.psu = "adjust" )
+
 library(survey)
 
-pof_df <- readRDS( file.path( getwd() , "2015 main.rds" ) )
+poststr <- 
+	readRDS( 
+		file.path( getwd() , 
+			"2008_2009/poststr.rds" ) 
+		)
 
-pof_design <- 
-	svydesign( 
-		~ psu , 
-		strata = ~ stratum , 
-		data = pof_df , 
-		weights = ~ weight , 
-		nest = TRUE 
+		
+t_morador_s <- 
+	readRDS( 
+		file.path( getwd() , 
+			"2008_2009/t_morador_s.rds" ) 
+		)
+
+t_morador_s <-
+	transform(
+		t_morador_s , 
+		control = paste0( cod_uf , num_seq , num_dv ) 
+	)
+	
+pof_df <- merge( t_morador_s , poststr )
+
+stopifnot( nrow( pof_df ) == nrow( t_morador_s ) )
+
+pre_stratified_df <- 
+	svydesign(
+		id = ~control , 
+		strata = ~estrato_unico ,
+		weights = ~fator_expansao1 ,
+		data = pof_df ,
+		nest = TRUE
+	)
+
+population_totals <- 
+	data.frame(
+		pos_estrato = unique( pre_stratified_df$pos_estrato ) , 
+		Freq = unique( pre_stratified_df$tot_pop ) 
+	)
+
+pof_design <-
+	postStratify(
+		pre_stratified_df , 
+		~pos_estrato , 
+		population_totals
 	)
 pof_design <- 
-	update( 
+	update(
 		pof_design , 
-		q2 = q2 ,
-		never_rarely_wore_bike_helmet = as.numeric( qn8 == 1 ) ,
-		ever_smoked_marijuana = as.numeric( qn47 == 1 ) ,
-		ever_tried_to_quit_cigarettes = as.numeric( q36 > 2 ) ,
-		smoked_cigarettes_past_year = as.numeric( q36 > 1 )
+		
+		one = 1 ,
+		
+		# centimeters instead of meters
+		altura_imputado = altura_imputado / 100 ,
+		
+		age_categories =
+			factor( 
+				1 + findInterval( idade_anos , 
+					c( 20 , 25 , 30 , 35 , 45 , 55 , 65 , 75 ) ) ,
+				levels = 1:9 , labels = c( "under 20" , "20-24" , "25-29" ,
+				"30-34" , "35-44" , "45-54" , "55-64" , "65-74" , "75+" )
+			) ,
+		
+		# create a body mass index (bmi) variable, excluding babies (who have altura_imputado==0)			
+		body_mass_index = ifelse( altura_imputado == 0 , 0 , peso_imputado / ( altura_imputado ^ 2 ) ) ,
+		
+		sexo = ifelse( cod_sexo == '01' , "masculino" , ifelse( cod_sexo == '02' , "feminino" , NA ) )
+		
+		
+	)
+
+pof_design <-
+	transform(
+		pof_design ,
+		
+		# individuals with a low bmi - underweight
+		underweight = ifelse( body_mass_index < 18.5 , 1 , 0 ) ,
+		
+		# individuals with a high bmi - overweight
+		overweight = ifelse( body_mass_index >= 25 , 1 , 0 ) ,
+		
+		# individuals with a very high bmi - obese
+		obese = ifelse( body_mass_index >= 30 , 1 , 0 )
 	)
 sum( weights( pof_design , "sampling" ) != 0 )
 
-svyby( ~ one , ~ ever_smoked_marijuana , pof_design , unwtd.count )
+svyby( ~ one , ~ sexo , pof_design , unwtd.count )
 svytotal( ~ one , pof_design )
 
-svyby( ~ one , ~ ever_smoked_marijuana , pof_design , svytotal )
-svymean( ~ bmipct , pof_design , na.rm = TRUE )
+svyby( ~ one , ~ sexo , pof_design , svytotal )
+svymean( ~ body_mass_index , pof_design , na.rm = TRUE )
 
-svyby( ~ bmipct , ~ ever_smoked_marijuana , pof_design , svymean , na.rm = TRUE )
-svymean( ~ q2 , pof_design , na.rm = TRUE )
+svyby( ~ body_mass_index , ~ sexo , pof_design , svymean , na.rm = TRUE )
+svymean( ~ age_categories , pof_design )
 
-svyby( ~ q2 , ~ ever_smoked_marijuana , pof_design , svymean , na.rm = TRUE )
-svytotal( ~ bmipct , pof_design , na.rm = TRUE )
+svyby( ~ age_categories , ~ sexo , pof_design , svymean )
+svytotal( ~ body_mass_index , pof_design , na.rm = TRUE )
 
-svyby( ~ bmipct , ~ ever_smoked_marijuana , pof_design , svytotal , na.rm = TRUE )
-svytotal( ~ q2 , pof_design , na.rm = TRUE )
+svyby( ~ body_mass_index , ~ sexo , pof_design , svytotal , na.rm = TRUE )
+svytotal( ~ age_categories , pof_design )
 
-svyby( ~ q2 , ~ ever_smoked_marijuana , pof_design , svytotal , na.rm = TRUE )
-svyquantile( ~ bmipct , pof_design , 0.5 , na.rm = TRUE )
+svyby( ~ age_categories , ~ sexo , pof_design , svytotal )
+svyquantile( ~ body_mass_index , pof_design , 0.5 , na.rm = TRUE )
 
 svyby( 
-	~ bmipct , 
-	~ ever_smoked_marijuana , 
+	~ body_mass_index , 
+	~ sexo , 
 	pof_design , 
 	svyquantile , 
 	0.5 ,
@@ -63,14 +128,14 @@ svyby(
 	na.rm = TRUE
 )
 svyratio( 
-	numerator = ~ ever_tried_to_quit_cigarettes , 
-	denominator = ~ smoked_cigarettes_past_year , 
+	numerator = ~ peso_imputado , 
+	denominator = ~ altura_imputado , 
 	pof_design ,
 	na.rm = TRUE
 )
-sub_pof_design <- subset( pof_design , qn41 == 1 )
-svymean( ~ bmipct , sub_pof_design , na.rm = TRUE )
-this_result <- svymean( ~ bmipct , pof_design , na.rm = TRUE )
+sub_pof_design <- subset( pof_design , underweight == 1 )
+svymean( ~ body_mass_index , sub_pof_design , na.rm = TRUE )
+this_result <- svymean( ~ body_mass_index , pof_design , na.rm = TRUE )
 
 coef( this_result )
 SE( this_result )
@@ -79,8 +144,8 @@ cv( this_result )
 
 grouped_result <-
 	svyby( 
-		~ bmipct , 
-		~ ever_smoked_marijuana , 
+		~ body_mass_index , 
+		~ sexo , 
 		pof_design , 
 		svymean ,
 		na.rm = TRUE 
@@ -91,22 +156,22 @@ SE( grouped_result )
 confint( grouped_result )
 cv( grouped_result )
 degf( pof_design )
-svyvar( ~ bmipct , pof_design , na.rm = TRUE )
+svyvar( ~ body_mass_index , pof_design , na.rm = TRUE )
 # SRS without replacement
-svymean( ~ bmipct , pof_design , na.rm = TRUE , deff = TRUE )
+svymean( ~ body_mass_index , pof_design , na.rm = TRUE , deff = TRUE )
 
 # SRS with replacement
-svymean( ~ bmipct , pof_design , na.rm = TRUE , deff = "replace" )
-svyciprop( ~ never_rarely_wore_bike_helmet , pof_design ,
+svymean( ~ body_mass_index , pof_design , na.rm = TRUE , deff = "replace" )
+svyciprop( ~ obese , pof_design ,
 	method = "likelihood" , na.rm = TRUE )
-svyttest( bmipct ~ never_rarely_wore_bike_helmet , pof_design )
+svyttest( body_mass_index ~ obese , pof_design )
 svychisq( 
-	~ never_rarely_wore_bike_helmet + q2 , 
+	~ obese + age_categories , 
 	pof_design 
 )
 glm_result <- 
 	svyglm( 
-		bmipct ~ never_rarely_wore_bike_helmet + q2 , 
+		body_mass_index ~ obese + age_categories , 
 		pof_design 
 	)
 
@@ -114,17 +179,9 @@ summary( glm_result )
 library(srvyr)
 pof_srvyr_design <- as_survey( pof_design )
 pof_srvyr_design %>%
-	summarize( mean = survey_mean( bmipct , na.rm = TRUE ) )
+	summarize( mean = survey_mean( body_mass_index , na.rm = TRUE ) )
 
 pof_srvyr_design %>%
-	group_by( ever_smoked_marijuana ) %>%
-	summarize( mean = survey_mean( bmipct , na.rm = TRUE ) )
-
-unwtd.count( ~ never_rarely_wore_bike_helmet , yrbss_design )
-
-svytotal( ~ one , subset( yrbss_design , !is.na( never_rarely_wore_bike_helmet ) ) )
- 
-svymean( ~ never_rarely_wore_bike_helmet , yrbss_design , na.rm = TRUE )
-
-svyciprop( ~ never_rarely_wore_bike_helmet , yrbss_design , na.rm = TRUE , method = "beta" )
+	group_by( sexo ) %>%
+	summarize( mean = survey_mean( body_mass_index , na.rm = TRUE ) )
 
